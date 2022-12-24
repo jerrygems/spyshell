0ba805c034691369e9d7e97030c2b726bf24ae99
J3rry <100356532+jerrygems@users.noreply.github.com>
2022-12-24 19:29:27 +0000

    Update initfun.sh

--- initfun.sh
+++ initfun.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 echo "<----------installing in background, Have some popcorns till then---------->"
-run="sudo apt install nyancat && sudo apt install sl && sudo apt install cmatrix && sudo apt isntall figlet & 2>/dev/null" 
+run="sudo apt install nyancat && sudo apt install sl && sudo apt install cmatrix && sudo apt install figlet" 
+eval ${run} &> /dev/null &
+
+pid=$!
+while kill -0 ${pid} &> /dev/null 
+do
+
+	echo -n "="
+	sleep 1
+done
+echo -n ">"
+echo ""
+echo "<----------thanks for having patience---------->"
+
+on 
