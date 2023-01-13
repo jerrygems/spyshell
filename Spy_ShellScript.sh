32db32bee4f2b8d0c830f5365e9ff0bad97bfd07
J3rry <100356532+jerrygems@users.noreply.github.com>
2023-01-13 17:12:02 +0530

    Update Spy_ShellScript.sh

--- Spy_ShellScript.sh
+++ Spy_ShellScript.sh
@@ -53,13 +53,13 @@ do
         if [[ ${cmd} == "hello" ]]
         then
 
-            echo -e "${yellow}welcome to ${nc}${red}Sp1d3y's${nc}${yellow} naughty shell${nc}"
-            echo -n -e "${boldRED}"
-            while :; do clear;echo -e "${first}" ; sleep 0.1;clear; echo -e "${second}" ; sleep 0.1;clear; echo -e "${third}" ; sleep 0.1;clear; echo -e "${fourth}" ; sleep 0.1; break; done;
-            while :; do clear;echo -e "${first}" ; sleep 0.1;clear; echo -e "${second}" ; sleep 0.1;clear; echo -e "${third}" ; sleep 0.1;clear; echo -e "${fourth}" ; sleep 0.1; break; done;
-            while :; do clear;echo -e "${first}" ; sleep 0.1;clear; echo -e "${second}" ; sleep 0.1;clear; echo -e "${third}" ; sleep 0.1;clear; echo -e "${fourth}" ; sleep 0.1; break; done;
-            figlet -f small.flf "${cmd} $(whoami)"
-            echo -n -e "${nc}"
+                echo -e "${yellow}welcome to ${nc}${red}Sp1d3y's${nc}${yellow} naughty shell${nc}"
+                echo -n -e "${boldRED}"
+                while :; do clear;echo -e "${first}" ; sleep 0.1;clear; echo -e "${second}" ; sleep 0.1;clear; echo -e "${third}" ; sleep 0.1;clear; echo -e "${fourth}" ; sleep 0.1; break; done;
+                while :; do clear;echo -e "${first}" ; sleep 0.1;clear; echo -e "${second}" ; sleep 0.1;clear; echo -e "${third}" ; sleep 0.1;clear; echo -e "${fourth}" ; sleep 0.1; break; done;
+                while :; do clear;echo -e "${first}" ; sleep 0.1;clear; echo -e "${second}" ; sleep 0.1;clear; echo -e "${third}" ; sleep 0.1;clear; echo -e "${fourth}" ; sleep 0.1; break; done;
+                figlet -f small.flf "${cmd} $(whoami)"
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "initfun" || ${cmd} == "InitFun" || ${cmd} == "ini" || ${cmd} == "INITFUN" ]]
         then
                 echo -n -e "${purple}"
@@ -74,59 +74,59 @@ do
                 echo -n -e "${nc}"
         elif [[ ${cmd} == "time" ]]
         then
-            echo -n -e "${purple}"
-            date | awk '{print $4,$5,$6}' | figlet
-            echo -n -e "${nc}"
+                echo -n -e "${purple}"
+                date | awk '{print $4,$5,$6}' | figlet
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "date" ]]
         then
-            echo -n -e "${purple}"
-            date | awk '{print $3,$2,$7}' | figlet
-            echo -n -e "${nc}"
+                echo -n -e "${purple}"
+                date | awk '{print $3,$2,$7}' | figlet
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "whoami" ]]
         then
-            echo -n -e "${purple}"
-            ${cmd} | figlet
-            echo -n -e "${nc}"
+                echo -n -e "${purple}"
+                ${cmd} | figlet
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "hostname" ]]
         then
-            echo -n -e "${purple}"
-            ${cmd} | figlet
-            echo -n -e "${nc}"
+                echo -n -e "${purple}"
+                ${cmd} | figlet
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "exit" || ${cmd} == "bye" ]]
         then
-            echo -n -e "${boldRED}"
-            figlet -f small.flf "good bye $(whoami)"
-            exit
-            echo -n -e "${nc}"
+                echo -n -e "${boldRED}"
+                figlet -f small.flf "good bye $(whoami)"
+                exit
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "grabip" ]]
         then
-            #print colored output2>/dev/null && ${cmd} 2>spyshell.log
-            echo -n -e "${boldRED}"
-            ifconfig | grep -E "inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | awk '{print $1,$2}' | figlet -f small.flf
-            echo -n -e "${nc}"
+                #print colored output2>/dev/null && ${cmd} 2>spyshell.log
+                echo -n -e "${boldRED}"
+                ifconfig | grep -E "inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | awk '{print $1,$2}' | figlet -f small.flf
+                echo -n -e "${nc}"
         elif [[ ${cmd} == "spylistner" ]]
         then
-            echo "spylistner will be here" | figlet
+                echo "spylistner will be here" | figlet
         elif [[ ${cmd} == "COT" ]]
         then
-            echo "chat over terminal" | figlet
+                echo "chat over terminal" | figlet
         elif [[ ${cmd} == "bash-compline" ]]
         then
-            echo "bash completion without tab" | figlet
+                echo "bash completion without tab" | figlet
         elif [[ ${cmd} == "koth setup" ]]
         then
-            echo "starting koth setup" | figlet
-#koth automation
+                echo "starting koth setup" | figlet
+                #koth automation
         elif [[ ${cmd} == "hello-friend" ]]
         then
-            read -e -r -p "enter path of ovpn file : " path
-            nohup sudo openvpn ${path} 2>/dev/null &
+                read -e -r -p "enter path of ovpn file : " path
+                nohup sudo openvpn ${path} 2>/dev/null &
         elif [[ ${cmd} ]]
         then
-            #print colored output2>/dev/null && ${cmd} 2>spyshell.log
-            echo -n -e "${purple}"
-            eval "${cmd}"
-            echo -n -e "${nc}"
+                #print colored output2>/dev/null && ${cmd} 2>spyshell.log
+                echo -n -e "${purple}"
+                eval "${cmd}"
+                echo -n -e "${nc}"
         fi
 
 
